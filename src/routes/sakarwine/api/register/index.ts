176a46c2b255e4ec63e261be615b2787a7584fb9
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro-2.local>
2023-04-24 20:29:20 +0630

    update

--- src/routes/sakarwine/api/register/index.ts
+++ src/routes/sakarwine/api/register/index.ts
@@ -1,6 +1,23 @@
 import { brewBlankExpressFunc, throwErrorResponse } from "code-alchemy";
 import bcrypt from "bcrypt";
-import User, { IUser } from "../../../../models/User";
+import validator from "validator";
+import User from "../../../../models/User";
+
+const emailIsValid = (email: string) => {
+  return validator.isEmail(email);
+};
+
+const passwordIsValid = (password: string) => {
+  // Minimum 8 characters, at least 1 letter and 1 number
+  const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+  return regex.test(password);
+};
+
+const usernameIsValid = (username: string) => {
+  // Minimum 3 characters, maximum 30 characters, alphanumeric
+  const regex = /^[a-zA-Z0-9]{3,30}$/;
+  return regex.test(username);
+};
 
 export default brewBlankExpressFunc(async (req, res) => {
   const {
@@ -14,6 +31,24 @@ export default brewBlankExpressFunc(async (req, res) => {
     language,
     timezone,
   } = req.body;
+
+  if (!emailIsValid(email)) {
+    throwErrorResponse(400, "Invalid email format");
+  }
+
+  if (!passwordIsValid(password)) {
+    throwErrorResponse(
+      400,
+      "Password must have at least 8 characters, including at least 1 letter and 1 number"
+    );
+  }
+
+  if (!usernameIsValid(username)) {
+    throwErrorResponse(
+      400,
+      "Username must be 3-30 characters long and contain only alphanumeric characters"
+    );
+  }
 
   // Check if the user already exists
   const existingUser = await User.findOne({ email });
